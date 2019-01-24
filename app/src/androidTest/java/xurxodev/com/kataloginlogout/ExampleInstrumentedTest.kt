d6d73789b768971a503c8980d5c1e835dd41cb6e
Jorge Sánchez <xurxosf@gmail.com>
2019-01-24 15:55:30 +0100

    Add initial files

---
+++ app/src/androidTest/java/xurxodev/com/kataloginlogout/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package xurxodev.com.kataloginlogout
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("xurxodev.com.kataloginlogout", appContext.packageName)
+    }
+}
